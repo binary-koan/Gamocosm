d347b6c7fbcd4ad0cfa37003de559aba9f4ea5f6
Jono Mingard <reason.koan@gmail.com>
2016-11-03 14:33:52 +1300

    Add extra debug logging

--- app/workers/setup_server_worker.rb
+++ app/workers/setup_server_worker.rb
@@ -30,6 +30,7 @@ class SetupServerWorker
   def perform(server_id, times = 0)
     server = Server.find(server_id)
     user = server.user
+    server.log("Setting up server ...")
     begin
       if !server.remote.exists?
         server.log('Error starting server; remote_id is nil. Aborting')
